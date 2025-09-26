906bb5a5a36bdaa807ee0934497e97494b1bdb2b
ObamaProschai <litvinenergy001@gmail.com>
2025-09-26 11:35:47 +0300

    Написали метод поиска findBook

--- my_library.js
+++ my_library.js
@@ -1,43 +1,45 @@
 class Library {
     constructor(libTitle) {
-        this.libraryTitle
-        this.books = {};
-        this.totalBooks = 0;    // общее кол-во книг
-        this.borrowedBooks = 0; // кол-во выданныйх книг
+        this.libraryTitle = libTitle;          // Присваиваем значение
+        this.books = [];                      // books теперь массив
+        this.totalBooks = 0;                  // Общее количество книг
+        this.borrowedBooks = 0;               // Количество выданных книг
     }
 
-    // Добавим новую книгу,Если `isnb` уже есть в базе,
-    // То увеличим кол-во книг. (поле `quantite`)
-    addBook(title, author, year, isnb, quantity = 1){
+    addBook(title, author, year, isnb, quantity = 1) {
         const newBook = {
             title: title,
             author: author,
             isnb: isnb,
             year: year,
-            totalQuantity: quantity,    // кол-во экземпляров
-            availableQuantity: quantity,    // кол-во доступных для выдачи
-            borrowedBy: []  // список читателей кто брал книгу
-        }
-        const findBook = this.books.filter(book => book.isbn == isbn);
-        if (findBook.length){
-            for (var i = 0; i <= this.books.lengt; i++) {
-                if (this.book == findBook[0]) {
-                    this.books[i].totalQuantity += quantity;
-                    this.books[i].availableQuantity += quantity;
-                }
-            }
+            totalQuantity: quantity,          // Кол-во экземпляров
+            availableQuantity: quantity,      // Доступных экземпляров
+            borrowedBy: []
+        };
+        
+        const findBook = this.books.find(book => book.isnb === isnb); // Используем .find()
+        
+        if (findBook) {
+            findBook.totalQuantity += quantity;
+            findBook.availableQuantity += quantity;
         } else {
-            this.books.push(newBook)
+            this.books.push(newBook);
         }
         return newBook;
     }
-
+    
+    findBook(searchTerm) {
+        return this.books.filter(book => book.title.includes(searchTerm)); // Исправленное условие
+    }
 }
+
 const library = new Library("Тестовая библиотека");
 
-// Добавляем книги 
 library.addBook("JavaScript для начинающих", "Иван Петров", 2023, "JS-001", 5);
 library.addBook("React продвинутый", "Мария Сидорова", 2024, "REACT-002", 3);
 library.addBook("React продвинутый", "Мария Сидорова", 2024, "REACT-002", 1);
+
 console.log(library.books);
 
+const foundBooks = library.findBook("JavaScript");
+console.log(foundBooks.length); // Вернет 1
